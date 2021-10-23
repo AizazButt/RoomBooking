8b192d21327d9f7c0ce3b7d24e525b9c602f02ba
jaju00 <aizazbutt.22@gmail.com>
2021-10-23 18:01:33 +0500

    Updated Hotel

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 const reader = require("readline-sync"); //npm install readline-sync
-
-// let a = parseInt(reader.question("Enter First Value: "))
-// let b = parseInt(reader.question("Enter Second Value: "))
-
 let roomsMeta = [
     {
         number: 1,
@@ -47,21 +43,58 @@ function dashboardScreen() {
         }
     }
 }
-
 function showBookedRoomsScreen() {
     while (true) {
         const bookedRooms = roomsMeta.filter(room => room.booked)
+
+
         if (bookedRooms.length === 0) {
             console.log("No Booked Rooms Yet!")
             break;
         }
-        bookedRooms.forEach(room => {
+        bookedRooms.forEach(room =>
             console.log(`Room ${room.number} is Booked.`)
-        })
+        )
+       CheckOutScreen();
+    }
+}
+function CheckOutScreen() {
+    while (true) {
+        console.log("1. To Check Out from Booked Rooms")
+        console.log("0. Exit")
+
+        const checkroom = parseInt(reader.question("Enter Value: "));
+        if (checkroom === 0) {
+            dashboardScreen()
+            break;
+        } else if (checkroom === 1) {
+            ToCheckOutRoom()
+        }
+    }
+}
+function ToCheckOutRoom(){
+    while(true){
+        const roomToCheckOut = parseInt(reader.question("Enter Room Number To CheckOut: "));
+        const room = roomsMeta.filter(room => room.number === roomToCheckOut);
+
+        if (room.length === 0) {
+            console.log("Please Enter a valid value!")
+            continue
+        }
+        let newRooms = roomsMeta.filter(room => room.number !== roomToCheckOut)
+
+        console.log(`Room ${room[0].number} is now Available!`)
+        room[0] = {
+            ...room[0],
+            booked: false
+        }
+        roomsMeta = [
+            ...newRooms,
+            room[0]
+        ]
         break
     }
 }
-
 function showAvailableRooms() {
     while (true) {
         const availableRooms = roomsMeta.filter(room => !room.booked)
@@ -73,12 +106,16 @@ function showAvailableRooms() {
             console.log(`Room ${room.number} is Available.`)
         })
         const roomToBook = parseInt(reader.question("Enter Room Number To Book: "));
+
+
         const room = roomsMeta.filter(room => room.number === roomToBook); // Array []
         if (room.length === 0) {
             console.log("Please Enter a valid value!")
             continue
         }
         let newRooms = roomsMeta.filter(room => room.number !== roomToBook)
+
+        console.log(`Room ${room[0].number} has been Booked!`)
         room[0] = {
             ...room[0],
             booked: true
@@ -87,7 +124,6 @@ function showAvailableRooms() {
             ...newRooms,
             room[0]
         ]
-        console.log(`Room ${room[0].number} has been Booked!`)
         break
     }
 }
